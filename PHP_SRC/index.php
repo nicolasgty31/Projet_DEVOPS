3f47154d52a96cb93c4800f8ba53359fb3975aa5
mklevesque <mxchaellevesque@gmail.com>
2021-12-13 17:29:41 +0100

    add database setting

--- PHP_SRC/index.php
+++ PHP_SRC/index.php
@@ -1,15 +1,15 @@
 
 <?php
-#$user = NULL;
-#$pass = NULL;
+$user = root;
+$pass = password;
 
-#$bdd = new PDO('mysql:host=localhost;dbname=bdd', $user, $pass);
+$bdd = new PDO('mysql:host=10.103.174.63:3306;dbname=database', $user, $pass);
 
-#$projects = $bdd->query('SELECT * FROM projet');
+$projects = $bdd->query('SELECT * FROM projet');
 
-#$users = $bdd->query('SELECT * FROM user');
+$users = $bdd->query('SELECT * FROM user');
 
-#$project = $projects->fetch()
+$project = $projects->fetch()
 
 ?>
 
@@ -23,14 +23,14 @@
     <h1> Projet Devops</h1>
 </div>
 <div>
-    <h2>Notre projet(pas celui de macron) :<?php #$project['nom'] ?> </h2>
-    <p> <?php #$project->description ?> </p>
+    <h2>Notre projet(pas celui de macron) :<?php $project['nom'] ?> </h2>
+    <p> <?php $project->description ?> </p>
     <h2>Les participants</h2>
     <ul>
-         <?php #while ($user = $users->fetch()) { ?>
-        <li> nom : <?php #$user['nom'] ?>, prenom :  <?php #$user['prenom'] ?></li>
-         <?php #}
-         #$users->closeCursor();?>
+         <?php while ($user = $users->fetch()) { ?>
+        <li> nom : <?php $user['nom'] ?>, prenom :  <?php $user['prenom'] ?></li>
+         <?php}
+         $users->closeCursor();?>
     </ul>
 </div>
 
